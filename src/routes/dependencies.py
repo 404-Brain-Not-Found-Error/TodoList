9a16bbe13ffafa84a96b6af3c6ae9e5cb1131b68
marikkaratist <nebose@mail.ru>
2025-03-30 09:52:28 +0700

    Исправление dependencies.py

--- src/routes/dependencies.py
+++ src/routes/dependencies.py
@@ -2,11 +2,12 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from src.database import async_session_maker
 from utils.db_manager import DBManager
 
 
 async def get_db():
-    async with DBManager(session_factory=async_sessionmaker) as db:
+    async with DBManager(session_factory=async_session_maker) as db:
         yield db
 
 DBDep = Annotated[DBManager, Depends(get_db)]

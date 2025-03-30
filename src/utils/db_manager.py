36235c52a05c5afa5644f921c0c6f231c67458ec
marikkaratist <nebose@mail.ru>
2025-03-30 10:15:20 +0700

    Импорт tasks-реапозитория в db_manager.py

--- src/utils/db_manager.py
+++ src/utils/db_manager.py
@@ -1,3 +1,5 @@
+from src.repositories.tasks import TasksRepository
+
 
 class DBManager:
     def __init__(self, session_factory):
@@ -5,6 +7,8 @@ class DBManager:
 
     async def __aenter__(self):
         self.session = self.session_factory()
+
+        self.tasks = TasksRepository(self.session)
 
         return self
 
